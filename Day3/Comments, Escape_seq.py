d077869064460518b87f5b50cbf6ef5cd68dac50
roshaniishrestha <roshani123shrestha@gmail.com>
2024-06-03 17:58:20 +0545

    basics

---
+++ Day3/Comments, Escape_seq.py
@@ -0,0 +1,5 @@
+#first comments in python
+print("this is first line \nthis is second line") #this is second comment 
+print('Hey i am\' a \"good girl\" \nthis is how we write word in single quote and double quote using \'slash\'')
+print("hey", 0,1,1,0, sep="~", end="111\n")
+print("ROSHANI")
---
+++ Day4/variable_&_data_type.py
@@ -0,0 +1,10 @@
+a = 12345
+print("the type of a is", (type(a)))
+b = "roshani"
+print("the type of b is", (type(b)))
+c = 1.1
+print ("the type of c is", (type(c)))
+d = True
+print ("the type of d is", (type(d)))
+e = None
+print ("the type of e is", (type(e)))
