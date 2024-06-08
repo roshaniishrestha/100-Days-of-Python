26a20c6abf2c5f6c6fb3eb97eb170148a3e5bb6f
roshaniishrestha <roshani123shrestha@gmail.com>
2024-06-08 23:56:14 +0545

    Loop in python

---
+++ Day13/forloop.py
@@ -0,0 +1,23 @@
+x = "roshani"
+for i in x:
+    print (i, end=", ")
+    
+colors = ["\n", "red", "blue", "pink"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i,"\n") 
+
+for k in range(5): #range is a built in class
+    print(k, end=", ") #result -> 0, 1, 2, 3, 4,
+print()
+
+for k in range(5): #range is a built in class
+    print(k+1, end=", ") #result ->  1, 2, 3, 4,5,
+print()
+
+for r in range(2,7):
+    print(r, end=", ")
+print()
+for p in range (1, 10, 3):
+    print(p, end=", ")
