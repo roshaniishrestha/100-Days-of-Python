226383cdf81982e3a8d242393303bfe2216796ef
roshaniishrestha <roshani123shrestha@gmail.com>
2024-06-08 22:39:26 +0545

    Good Morning Timestamp

---
+++ Day11/Task1.py
@@ -0,0 +1,13 @@
+#programs that says - Good morning with time stamp
+#strf is a function that gives us time
+import time
+timestamp = time.strftime('%H:%M:%S')
+# print(timestamp)
+# timestamp = time.strftime("%H")
+# print(timestamp)
+# timestamp = time.strftime("%M")
+# print(timestamp)
+# timestamp = time.strftime("%S")
+# print(timestamp)
+
+print("Good Morning", timestamp)
